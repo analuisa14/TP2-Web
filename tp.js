c53e1d25eb4be258bfb767fcabd28ac4e2ada815
analuisa14 <40270890+analuisa14@users.noreply.github.com>
2018-10-08 23:21:20 -0300

    Add files via upload

---
+++ tp.js
@@ -0,0 +1,19 @@
+let $chat = $('#chat');
+let $box = $('#box');
+
+function criaMensagem(lado, texto) {
+	let $mensagem = $('<p class="mensagem ' + lado + '"></p>').hide();
+  $mensagem.html(texto);
+  $chat.append($mensagem);
+  $mensagem.show(100);
+}
+
+setTimeout(function() {criaMensagem('esquerda', 'Ola!');}, 250);
+
+$box.keyup(function(e) {
+	if(e.which == 13 && $box.val() != '') {
+  	criaMensagem('direita', $box.val());
+    $box.val('');
+  }
+});
+
